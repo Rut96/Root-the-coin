cb308d27d5aef37bdc224bfa0ddefb72cf268c14
Rut <vanrut042@gmail.com>
2024-11-23 16:59:41 +0200

    coins fix 8

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
     BASE_URL: window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
         ? ""
-        : "/Root-the-coin"
+        : "/Root-the-coin"  
 };
